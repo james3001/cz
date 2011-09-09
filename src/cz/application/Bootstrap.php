7a8c1a4e05a1da94430643f2290a57b9da33fbeb
Curso Zend <ernestex+cursozend@gmail.com>
2011-09-08 23:08:03 -0500

    layout

--- src/cz/application/Bootstrap.php
+++ src/cz/application/Bootstrap.php
@@ -2,6 +2,18 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    
+    public function _initView() {
+        
+        $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
+        $view = $layout->getView();
+        $view->headTitle('CZ');
+        $view->headTitle('Home');
+        $view->headTitle()->setSeparator(' | ');
+        
+        
+    }
 
 
 }
