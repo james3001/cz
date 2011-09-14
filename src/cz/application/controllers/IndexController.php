f218cd555f84addadcefe043b0505bd6ea872e2c
Curso Zend <ernestex+cursozend@gmail.com>
2011-09-13 21:47:46 -0500

    Firebug logger

--- src/cz/application/controllers/IndexController.php
+++ src/cz/application/controllers/IndexController.php
@@ -30,14 +30,35 @@ class IndexController extends Zend_Controller_Action
     {
         // config del logger
         $logger = new Zend_Log();
-        $writer = new Zend_Log_Writer_Stream(APPLICATION_PATH."/../logs/application.log");
-        $filter = new Zend_Log_Filter_Priority(Zend_log::ERR);
-        $logger->addFilter($filter);
-        $logger->addWriter($writer);
+        $writer1 = new Zend_Log_Writer_Stream(APPLICATION_PATH."/../logs/application.log");
+        $writer2 = new Zend_Log_Writer_Firebug();
+        $filter = new Zend_Log_Filter_Priority(Zend_Log::ERR);
+        $logger->addWriter($writer1);
+        $logger->addWriter($writer2);
+        //$logger->addFilter($filter);
+        
+        $data = array(1,2,3,array(7,8,9),"asdsa",5);
+        
+        $logger->debug($data);
+        $logger->debug($writer2);
+        
+//        
+//        $logger->log("emerg",  zend_log::EMERG);
+//        $logger->log("alert",  zend_log::ALERT);
+//        $logger->log("crit",  zend_log::CRIT);
+//        $logger->log("err",  zend_log::ERR);
+//        $logger->log("warn",  zend_log::WARN);
+//        $logger->log("not",  zend_log::NOTICE);
+//        $logger->log("info",  zend_log::INFO);
+//        $logger->log("debug",  zend_log::DEBUG);
+//        
+        
+        
+        
         
         // uso
-        $logger->log("Entraron a Contacto", Zend_Log::CRIT );
-        $logger->debug("Entraron a Contacto DBG");
+//        $logger->log("Entraron a Contacto (Firebug) ", Zend_Log::CRIT );
+//        $logger->debug("Entraron a Contacto DBG");
         
         
         $this->_helper->flashMessenger('Probando el flash messenger<br />');
