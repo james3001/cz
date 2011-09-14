8485de1b02b71d405a019ba0fc716544971100c8
Curso Zend <ernestex+cursozend@gmail.com>
2011-09-13 21:02:45 -0500

    sesion 4 - pre break

--- src/cz/application/Bootstrap.php
+++ src/cz/application/Bootstrap.php
@@ -11,8 +11,42 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view->headTitle('CZ');
         $view->headTitle('Home');
         $view->headTitle()->setSeparator(' | ');
+        $view->headMeta()->appendName('keywords','zend, curso zend, zend framework');
+        $view->headLink()->appendStylesheet('/css/global.css');
+        $view->headScript()->appendFile('/js/global.js','text/javascript');
+        $view->headScript()->appendFile(
+                '/js/fixie.js',
+                'text/javascript',
+                array('conditional'=>'lt IE 7')
+        );
         
+    }
+    
+    public function _initRoutes(){
         
+        $rutas = array(
+            'cntcto' => new Zend_Controller_Router_Route(
+                'contact-us',
+                array(
+                    'controller' => 'index',
+                    'action' => 'contacto'
+                )
+            ),
+            'ver-post' => new Zend_Controller_Router_Route(
+                'post/:id-:slug',
+                array(
+                    'module' => 'blog',
+                    'controller' => 'post',
+                    'action' => 'ver',
+                    'postId' => ':id',
+                    'postSlug' => ':slug'
+
+                )
+            )
+        );
+        
+        $this->frontController->getRouter()->addRoutes($rutas);
+
     }
 
 
--- src/cz/application/controllers/IndexController.php
+++ src/cz/application/controllers/IndexController.php
@@ -10,12 +10,38 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $fm = $this->_helper->flashMessenger;
+        $this->view->mensajes = $fm->getMessages();
+    }
+
+    public function configAction()
+    {
+        
+        $config = new Zend_Config_Ini(APPLICATION_PATH.'/configs/cz.ini');
+        $arr = $config->toArray();
+        var_dump($arr['redessociales']['facebook']['token']);
+        var_dump($config->redessociales->facebook->token);
+        echo "<hr>";
+        $configPag = new Zend_Config_Ini(APPLICATION_PATH.'/configs/cz.ini','paginadores');
+        var_dump($configPag->toArray());
     }
 
     public function contactoAction()
     {
-        // action body
+        // config del logger
+        $logger = new Zend_Log();
+        $writer = new Zend_Log_Writer_Stream(APPLICATION_PATH."/../logs/application.log");
+        //$log->addPriority($name, $priority);
+        $logger->addWriter($writer);
+        
+        // uso
+        $logger->log( __CLASS__ ."|". __METHOD__ . __LINE__ . "Entraron a Contacto", Zend_Log::ERR );
+        $logger->debug("Entraron a Contacto DBG");
+        
+        
+        $this->_helper->flashMessenger('Probando el flash messenger<br />');
+        //$this->_redirect('/');
+        
     }
 
 
