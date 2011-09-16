5548a7e6ab0a9984711157dfc3082caae648f673
Curso Zend <ernestex+cursozend@gmail.com>
2011-09-15 20:19:13 -0500

    MVC categoria, commit 1

--- src/cz/application/modules/ventas/controllers/CategoriaController.php
+++ src/cz/application/modules/ventas/controllers/CategoriaController.php
@@ -10,6 +10,13 @@ class Ventas_CategoriaController extends Zend_Controller_Action
 
     public function indexAction()
     {
+        $_categoria = new Application_Model_Categoria();
+        $db = $_categoria->getAdapter();
+        
+        $where = $db->quoteInto('activo = ?', 1);
+        
+        $this->view->categorias = $_categoria->fetchAll($where);
+        
     }
 
 }
